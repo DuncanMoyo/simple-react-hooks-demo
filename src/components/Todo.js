a5e1b8d8977651bc84eb59be7332878873030173
Duncan Moyo <duncanfmoyo@gmail.com>
2020-08-23 18:01:15 +0200

    using one state instead (not preferred)

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 
 const Todo = (props) => {
-  const [toDoName, setTodoName] = useState("");
-  const [toDoList, setToDoList] = useState([]);
+  // const [toDoName, setTodoName] = useState("");
+  // const [toDoList, setToDoList] = useState([]);
+
+  const [toDoState, setToDoState] = useState({ userInput: "", toDoList: [] });
 
   const inputChangeHandler = (event) => {
-    setTodoName(event.target.value);
+    // setTodoName(event.target.value);
+    setToDoState({
+      userInput: event.target.value,
+      toDoList: toDoState.toDoList,
+    });
   };
 
   const toDoAddHandler = () => {
-    setToDoList(toDoList.concat(toDoName));
+    // setToDoList(toDoList.concat(toDoName));
+    setToDoState({
+      userInput: toDoState.userInput,
+      toDoList: toDoState.toDoList.concat(toDoState.userInput),
+    });
   };
 
   return (
@@ -18,13 +28,13 @@ const Todo = (props) => {
         type="text"
         placeholder="Todo"
         onChange={inputChangeHandler}
-        value={toDoName}
+        value={toDoState.userInput}
       />
       <button type="button" onClick={toDoAddHandler}>
         Add
       </button>
       <ul>
-        {toDoList.map((todo) => (
+        {toDoState.toDoList.map((todo) => (
           <li key={todo}>{todo}</li>
         ))}
       </ul>
